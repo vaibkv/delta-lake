d362316be4a639508b4c814f343132241e15da46
Kumar Vaibhav <panghal.vaibhav@gmail.com>
2022-10-07 17:29:46 -0500

    First unit test

--- lake-operations/src/test/scala/lake_utils/post_trade_utils/TradesModelUtilsTest.scala
+++ lake-operations/src/test/scala/lake_utils/post_trade_utils/TradesModelUtilsTest.scala
@@ -1,11 +1,14 @@
 package lake_utils.post_trade_utils
 
+import io.delta.tables.DeltaTable
 import org.apache.spark.sql.SparkSession
 import org.junit.ClassRule
 import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.scalatest.{BeforeAndAfterAll, PrivateMethodTester}
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers._
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatestplus.junit.JUnitRunner
 import org.vaibhav.lake.test_utils.TestSparkSession
 
@@ -31,14 +34,17 @@ class TradesModelUtilsTest extends AnyFlatSpec with PrivateMethodTester with Tes
 
   it should "create initial table when first csv is available" in {
     //Arrange
+    val tableName = "tradesDeltaTable"
+    spark.sql(s"DROP TABLE IF EXISTS $tableName").show()
     val tradesTableLocation = temporaryFolder.newFolder().getAbsolutePath
     val csvLocation = this.getClass.getClassLoader.getResource("mock_data.csv").getPath
-    val utilsObj = TradesModelUtils("tradesDeltaTable", tradesTableLocation, true, csvLocation, None, spark)
+    val utilsObj = TradesModelUtils(tableName, tradesTableLocation, true, csvLocation, None, spark)
 
     //Act
     val resultDeltaTable = utilsObj.upsertTrades
 
     //Assert
-    val x = 2
+    resultDeltaTable.toDF.count() should be(1000L)
+    DeltaTable.isDeltaTable(spark, tradesTableLocation) should be(true)
   }
 }
